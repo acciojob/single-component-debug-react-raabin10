c7c8961e4cce06fcfe960188454e4c1877218d5b
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2024-02-12 19:14:49 +0000

    com

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ const App = ()=> {
 
 return(
   
-    <div>
-        <p>hello</p>
+    <div id="main">
+      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     </div>
 );
 };
